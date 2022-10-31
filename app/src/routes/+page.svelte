017b9bcc7f527de75e0413102b7d34801fdd446e
Joona Gynther <joona@gynther.xyz>
2022-10-31 02:00:38 +0200

    feat: add demo of noise generation to app

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import create_noise_function from "engine/map_gen/noise";
+  import { onMount } from "svelte";
+
+  const _noise = create_noise_function();
+  function noise(x: number, y: number) {
+    return _noise(x, y) / 2 + 0.5;
+  }
+
+  function octaves(x: number, y: number, octaves: number) {
+    let result = 0;
+    let freq = 0.01;
+    let amp = 1;
+    let maxAmp = 0;
+
+    for (let i = 0; i < octaves; ++i) {
+      result += noise(x * freq, y * freq) * amp;
+      maxAmp += amp;
+      amp /= 2;
+      freq *= 2;
+    }
+
+    result /= maxAmp;
+    return result;
+  }
+
+  const height = 250;
+  const width = 600;
+
+  onMount(() => {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    for (let y = 0; y < height; ++y) {
+      for (let x = 0; x < width; ++x) {
+        const value = octaves(x, y, 10) * 255;
+        context.fillStyle = `rgb(${value}, ${value}, ${value})`;
+        context.fillRect(x * 2, y * 2, 2, 2);
+      }
+    }
+  });
+</script>
+
+<canvas id="canvas" width={width * 2} height={height * 2} />
